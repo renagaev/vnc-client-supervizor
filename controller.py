c4216c6a4fe0612af52c1a054d94400fe0ea9a71
rnagaev68@gmail.com <BRf7wChy>
2020-12-05 13:06:03 +0300

    init

---
+++ controller.py
@@ -0,0 +1,60 @@
+from threading import Thread
+import subprocess
+import socket
+import time
+
+
+class Controller:
+    def __init__(self, host, port):
+        self.process = None
+        self.host = host
+        self.port = port
+        self.status = "inited"
+
+    def start(self):
+        Thread(target=self._start).start()
+
+    def host_reachable(self):
+        command = ["ping", "-c", self.host]
+        return subprocess.call(command) == 0
+
+    def port_reachable(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+
+        result = s.connect_ex((self.host, self.port)) == 0
+        s.close()
+        return result
+
+    def wait_host(self):
+        self.status = "wait host"
+        while True:
+            time.sleep(1)
+            reachable = self.host_reachable()
+            if reachable:
+                self.status = "host reachable"
+                break
+
+    def wait_port(self):
+        self.status = "wait port"
+        while True:
+            time.sleep(1)
+            reachable = self.port_reachable()
+            if reachable:
+                self.status = "port reachable"
+                break
+
+    def _start(self):
+        self.wait_host()
+        self.wait_port()
+        self.start_app()
+
+    def start_app(self):
+        command = []
+        self.process = subprocess.Popen(command)
+        self.status = "app active"
+
+    def force_restart(self):
+        if self.status == "app active":
+            self.process.terminate()
+        self.start()
---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from controller import Controller
+
+app = Flask(__name__)
+
+host = "192.168.0.109"
+port = 5900
+controller = Controller(host, port)
+
+controller.start()
+
+
+@app.route("/status")
+def get_status():
+    return controller.status
+
+
+@app.route("/restart")
+def restart():
+    controller.force_restart()
+    return "ok"
+
+
+app.run("0.0.0.0", 80)
